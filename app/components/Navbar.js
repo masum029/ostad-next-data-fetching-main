97bc93d9559b9fe48908f9238535a7c65fae201a
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2023-08-15 23:44:00 +0600

    first commit

---
+++ app/components/Navbar.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="flex space-x-6">
+            <div>
+                <Link href="/">Home</Link>
+            </div>
+
+            <div>
+                <Link href="/blog">Blog</Link>
+            </div>
+        </nav>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <section>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                autem, voluptas magni, consequatur nobis repellat dolores beatae
+                facilis illo consectetur in iusto minus ea dignissimos fugiat
+                dolorem ab recusandae optio?
+            </section>
+        </div>
+    );
+}
